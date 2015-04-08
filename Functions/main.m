3d8564ec77ef22d6a88f5cb38e75312c4bd80555
Vincent Renais <vincentrenais@gmail.com>
2015-04-08 10:57:32 -0400

    First commit

--- Functions/main.m
+++ Functions/main.m
@@ -8,10 +8,51 @@
 
 #import <Foundation/Foundation.h>
 
+// c style function
+void someFunction() {
+    NSLog(@"Some Function");
+}
+
+
+// 1-return type 2-name of function 3-parameters
+int returnSomeInt() {
+    return 5;
+}
+
+void addTwoSumVoid (int a, int b) {
+    int sum = a + b;
+    NSLog(@"%i", sum);
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        // call someFunction
+        someFunction();
+        int balance = returnSomeInt();
+        NSLog(@"%i", balance);
+        
+        int a = 10;
+        int b = 20;
+        
+        // call function with two integers
+        int c = 5;
+        int d = 5;
+        
+        addTwoSumVoid(25, 35);
+        addTwoSumVoid(c, d);
+
+        
+        // compare two integers
+        
+        if (a > b){
+            NSLog(@"value is larger");
+        } else {
+            NSLog(@"value isn't larger");
+        }
+        
+        
     }
     return 0;
 }
